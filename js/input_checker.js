eece60f918469cae8c7050906cd34df71fe4513c
Nick <nick@antinick>
2023-10-26 14:55:42 -0400

    >Implementing Boyer-Moore-Horspool. Half Done

--- js/input_checker.js
+++ js/input_checker.js
@@ -1,8 +1,43 @@
 function safetyCheck(val){
-    //While not at end of badwordlist.txt
-        //check from start to end for a bad word
+    xhtpp = new XMLHttpRequest();
+    xhtpp.open("GET", "../resources/badwordlist.txt")
+
+    xhtpp.onload = function(){
+
+        
+    }
             //if bad word return with error/box
 
             //no bad words call chatgptretriever
+}
 
+function search(pattern, inputtext){
+    t = preprocess(currentword);
+    skip = 0;
+    while(inputtext.length() - skip >= pattern.length){
+        if(same(inputtext, pattern, pattern.length()))
+            return skip;
+        skip += t[inputtext[skip + pattern.length() - 1]];
+    }
+    return false;
+}
+
+function same(str1, str2, len){
+    i = len -1
+    while (str1[i] = str2[i]){
+        if (i =0){
+            return true;
+        }
+        i--;
+    }
+    return false;
+}
+function preprocess(inputtext){
+    for( i=0; i < 256; i++){
+        t[i] = inputtext.length();
+    }
+    for (i = 0; i < inputtext.length()-1; i++){
+        t[inputtext[i]] = inputtext.length() - 1 - i;
+    }
+    return t;
 }
